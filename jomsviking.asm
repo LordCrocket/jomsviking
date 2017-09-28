cddc19dda1559e750d7ed9b8d899363b9efe77bc
rasmus <rasmus@larry>
2017-09-28 15:20:04 +0200

    Move town print to seperate subroutine
    The print_town subroutine which handles the formating of the output.
    
    Which could be simplified by a joint call

--- jomsviking.asm
+++ jomsviking.asm
@@ -11,9 +11,11 @@ main_menu:
 	db	" 0. Quit", 10
 	db	"====================================",10,0
     fmt db "%d",10, 0
+    fmt2 db "%d", 0
     
-    base_string db "Base:",0
-    town_string db "Town:",0
+    base_string db "Base: ",0
+    town_string db "Town ",0
+    colon db ": ",0
     
     base db 0
     fleet db 0
@@ -57,16 +59,10 @@ list_towns:
     jg print_block
     jmp next
 print_block:
-
-        mov rdi,town_string
-        call print
-
-        mov rdi,fmt
-        mov rax, towns
-        pop rcx
-        mov rsi, [rax + rcx]
-        push rcx
-        call print
+    pop rcx
+    mov rdi, rcx
+    push rcx
+    call print_town 
 next:
     pop rcx
     inc rcx
@@ -78,11 +74,41 @@ end_towns:
 	mov r13, rax
 
     inc byte [base]
-    inc byte [towns]
+    ;inc byte [towns]
 
     jmp while
 endwhile:
 
     mov     rax, 0            ; return back to C
     leave                     
+    ret
+
+
+;
+;   Print towns
+;
+
+print_town:
+    enter   0,0 
+    push rdi
+
+    mov rdi,town_string
+    call print
+
+    pop rcx
+    mov rdi,fmt2
+    mov rsi, rcx
+    inc rsi
+    push rcx
+    call print
+
+    mov rdi,colon
+    call print
+    
+    pop rcx
+    mov rdi,fmt
+    mov rax, towns
+    mov rsi, [rax + rcx]
+    call print
+    leave
     ret
