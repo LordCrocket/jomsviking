ae914a7d350a36f92206549f1c7ee0c1d0c142c8
rasmus <lordcrocket@yolotech.pl>
2017-10-30 21:39:56 +0100

    Moves some gamestate info to structs

--- src/jomsviking.asm
+++ src/jomsviking.asm
@@ -4,17 +4,35 @@ DEFAULT REL
 %include "src/logic.inc"
 %include "src/graphic.inc"
 
-
 segment .data
 
-    joms db 50
-    joms_gold db 50
-    attack db 0
+    struc   game_state
+        gs_joms:    resb 1
+        gs_j_gold:  resb 1
+        gs_player:  resq 1
+    endstruc
+
+    struc   player_state
+        p_gold:  resw 1
+        p_base:  resb 1
+        p_fleet: resb 1
+    endstruc
+
+    game: istruc game_state
+        at gs_joms, db  50
+        at gs_j_gold, db  50
+        at gs_player, dq  0
+    iend
+
+    player: istruc player_state
+        at p_gold, dw  0
+        at p_base, db  50
+        at p_fleet, db  10
+    iend
 
     work dw 0
-    gold dw 0
-    base db 50
-    fleet db 10
+    attack db 0
+
     towns times 8 db 0
 
     multiplier dq 25214903917
@@ -26,6 +44,11 @@ asm_main:
     enter   16,0 ; Make room for local loop variable
 
     mov qword [rsp],2
+
+    ; Link player to gamestate
+    lea rax ,[player]
+    mov [game+gs_player], rax
+
 
     call init_seed
 
@@ -40,16 +63,18 @@ while:
 
 
    ; Calculate new gold
-    mov rdi, base
+    mov rax, [game+gs_player]
+    lea rdi, [rax+p_base]
     mov rsi, towns
-    mov rdx, gold
+    lea rdx, [rax+p_gold]
     call generate_gold
 
 
     ; Find new workers
 
+    mov rax, [game+gs_player]
     mov rdi, towns
-    mov rsi, fleet
+    lea rsi, [rax+p_fleet]
     call find_workers
 
     ; Save info to display
@@ -58,7 +83,7 @@ while:
 
     ; Activate jomsviking
     mov rdi, towns
-    mov rsi, joms
+    lea rsi, [game+gs_joms]
     call active_jomsviking
 
     ; Save info to display
